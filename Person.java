27d088257415e359e82a6c8a02e7285b7dca16b1
Krzysztof <krzys.zolnowski@gmail.com>
2023-06-06 13:19:06 +0200

    commit - 5

--- Person.java
+++ Person.java
@@ -1,9 +1,12 @@
 public class Person {
+    private String name;
     public static void main(String[] args) {
         System.out.println("Hello!");
         int a = 3;
         String b = "abc";
         int c = 2;
+
+
     }
 
 }
